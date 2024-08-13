ee0576c87ce1f9846464a87ad432587b192adc4f
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-08-13 21:05:46 +0800

    0813

---
+++ localk_date/src/main/java/com/mozhimen/localk/date/LocalKDateUtil.kt
@@ -0,0 +1,12 @@
+package com.mozhimen.localk.date
+
+/**
+ * @ClassName LocalKDateUtil
+ * @Description TODO
+ * @Author mozhimen
+ * @Date 2024/8/13
+ * @Version 1.0
+ */
+object LocalKDateUtil {
+
+}
